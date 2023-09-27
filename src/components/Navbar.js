82cdbae38a9904ea75ec9640f0fc91e6fe269ef6
Aaskin345 <88335470+Aaskin345@users.noreply.github.com>
2023-09-27 13:38:03 +0300

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,6 @@ import logo from '../Assets/logo.png';
 
 import { Link } from 'react-router-dom';
 
-import { ImBlog } from 'react-icons/im';
 import {
   AiOutlineHome,
   AiOutlineFundProjectionScreen,
@@ -88,16 +87,6 @@ function NavBar() {
                 onClick={() => updateExpanded(false)}
               >
                 <CgFileDocument style={{ marginBottom: '2px' }} /> Resume
-              </Nav.Link>
-            </Nav.Item>
-
-            <Nav.Item>
-              <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <ImBlog style={{ marginBottom: '2px' }} /> Blogs
               </Nav.Link>
             </Nav.Item>
           </Nav>
